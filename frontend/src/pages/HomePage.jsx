b7e9e79cc098301fc74c5aeb27baa4980af44c12
Eyüp Öztürk <eyupoztrk04@gmail.com>
2025-07-06 15:49:12 +0300

    #2 homepage info message

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { fetchProducts } from '../services/api';
 import ProductCard from '../components/ProductCard';
-import '../styles/main.css'; 
+import '../styles/main.css';
 
 const HomePage = () => {
     const [products, setProducts] = useState([]);
@@ -22,7 +22,13 @@ const HomePage = () => {
 
     return (
         <div style={{ padding: '20px' }}>
-            <h1 style={{ textAlign: 'center', fontFamily: "Avenir"}}>Products</h1>
+            <h1 style={{ textAlign: 'center', fontFamily: "Avenir" }}>Products</h1>
+            <h1 style={{ textAlign: 'center', fontFamily: "Avenir", fontSize: 15 }}>
+                Backend deploy için https://render.com/ sitesini kullandım bu yüzden 15 dk boyunca istekler gelmez ise inaktif oluyor.
+                Lütfen 1-2 dk bekleyip sonra tekrar dener misiniz?<br />
+                Ayrıca Goldapi için goldapi.io sitesini kullandım ve bu da aylık 100 isteğe izin veriyor. Normalde çalışmaz ise sorun bundan kaynaklıdır.
+            </h1>
+
             <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', justifyContent: 'center' }}>
                 {products.map(product => (
 
